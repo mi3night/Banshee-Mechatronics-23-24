35d3848b5f5586d13a0e98a16c3b1da82493edea
Alexander Ov <60034450+OvAlexander@users.noreply.github.com>
2023-10-02 14:02:22 -0700

    Added versions 0.9 and 1.0
    Note Version 1.0 is still in progress please do not use yet

--- Arduino Code/QuicRun_WP_880_Dual_Brushed_Test_Code_V0_8/QuicRun_WP_880_Dual_Brushed_Test_Code_V0_8.ino
+++ Arduino Code/QuicRun_WP_880_Dual_Brushed_Test_Code_V0_8/QuicRun_WP_880_Dual_Brushed_Test_Code_V0_8.ino
@@ -1,13 +1,17 @@
-//Goal: Reformat the code to make it more efficent and add code to determine direction
+//Goal: Complete one half cycle
 //Created By: Alexander Ov
-//Created On: 9/20/23
-//Version 0.7
+//Created On: 9/25/23
+//Version 0.8
 //Updates: 
 /*
 //General Notes:
 "Forward" and "Backward" is arbitarty for now
 //Progress Report Notes
-
+Make sure 5V rail is powered by arduino
+ESC doesnt move in reverse
+70 going reverse with out rail is prefect
+now 70 doesnt work
+possibly a low battery lipo problem (not a lipo problem)
 */
 
 //Libraries
@@ -21,6 +25,7 @@ const int echoFront = 4;                  //ultrasonic sensor echo pin declerati
 const int trigBack = 7;                   //ultrasonic sensor trigBack pin decleration NOTE CHANGE PINS
 const int echoBack = 8;                   //ultrasonic sensor echoBack pin decleration NOTE CHANGE PINS
 const int led = 11;                       //led pin decleration
+const int led2 = 2;                       //TEMP LED
 
 //Object Decleration
 Servo servoLeft;                          //Create servo object to control the Battery Transfer Pod's left side DC Motors 
@@ -29,8 +34,6 @@ Servo servoRight;                         //Create servo object to control the B
 //Global Variable Decleration
 long duration;                            //duration var used for ultrasonic sensor calculations
 int distance;                             //distance var used for ultrasonic sensor calculations
-long duration2;                            //duration var used for ultrasonic sensor calculations
-int distance2;                             //distance var used for ultrasonic sensor calculations
 int direction;                            //direction var used to determine which way the motors should spin
 
 //Function Decleration
@@ -38,6 +41,7 @@ void senseWall();                         //Function to determine which ultrason
 
 //Setup Function
 void setup() {
+  Serial.println("Begin Setup");
   Serial.begin(9600);                     //Set baudrate to 9600
 
   servoLeft.attach(9, 1000, 2000);        //Attaches the motor driver on pin 9 to the servoLeft object (pin, min pulse width, max pulse width in microseconds)
@@ -46,24 +50,28 @@ void setup() {
   servoRight.attach(10, 1000, 2000);      //Attaches the motor driver on pin 10 to the servoRight object
   pinMode(10, OUTPUT);                    //declare pin 10 as OUTPUT (not needed?)
   pinMode(led, OUTPUT);                   //LED pin declerartion as OUTPUT
+  pinMode(led2, OUTPUT);                   //LED pin declerartion as OUTPUT
   pinMode(trigFront, OUTPUT);             //trigFront pin declerartion as OUTPUT
   pinMode(echoFront, INPUT);              //echoFront pin declerartion as OUTPUT
   pinMode(power, OUTPUT);                 //power pin declerartion as OUTPUT
 
   digitalWrite(led,HIGH);                 //Turn on LED for set up
+  digitalWrite(led2,HIGH);                //Turn on LED for reverse movement state
   digitalWrite(power, HIGH);              //Turn on power for set up
 
   servoLeft.write(90);                    //Set left side motors postion to 90
   servoRight.write(90);                   //Set right side motors postion to 90
   delay(5000);                            //Delay for 5 seconds
   digitalWrite(led,LOW);                  //Turn off LED for set up
-  direction = -1;
+  digitalWrite(led2,LOW);               //Turn on LED for reverse movement state
   Serial.println("Setup complete");       //Print to consle setup is complete
+  direction = 1;                          //Set direction var to forward
 }
 
 //Main Looping Fucntion
 void loop() {
   senseWall();                            //Call senseWall function
+  Serial.println(direction);              //Print to consle which direction is going
   if(direction == 1)                      //Check if direction is set as forward
   {
     digitalWrite(led,HIGH);               //Turn on LED for forward movement state
@@ -73,13 +81,14 @@ void loop() {
   }
   else if(direction == -1)                //Check if direction is set as backwards
   {
-    digitalWrite(led,HIGH);               //Turn on LED for reverse movement state
-    servoLeft.write(82);                  //Set left side motors postion to 82
-    servoRight.write(82);                 //Set right side motors postion to 82
+    digitalWrite(led2,HIGH);               //Turn on LED for reverse movement state
+    servoLeft.write(70);                  //Set left side motors postion to 82
+    servoRight.write(70);                 //Set right side motors postion to 82
     Serial.println("Moving reverse");     //Print to consle motors are moving backwards
   }
   else{                                   //Check if direction not forward or backward
     digitalWrite(led,LOW);                //Turn off LED for no movement state
+    digitalWrite(led2,LOW);               //Turn on LED for reverse movement state
     servoLeft.write(90);                  //Set left side motors postion to 82
     servoRight.write(90);                 //Set left side motors postion to 82
     Serial.println("Pod reset");          //Print to consle motors are reset
@@ -96,26 +105,27 @@ void senseWall()
     digitalWrite(trigFront, LOW);         //Turn off send trigger signal to front ultrasonic sensor
     duration = pulseIn(echoFront, HIGH);  //Set duration equal to the pulse reading from the echo of the front ultrasonic sensor
     distance = (duration/2)/29.1;         //Calculate the distance into cm
-    if(distance <= 10)                    //Check if the distance is less than or equal to 10 cm  from the front
+    if(distance < 10)                    //Check if the distance is less than or equal to 10 cm  from the front
     {
       direction = 0;                      //Set direction to none to trigger a stop
     }
   }
   else if(direction == -1)                //Check if direction is set as reverse
   {
-    digitalWrite (trigBack, HIGH);        //Turn on send trigger signal to back ultrasonic sensor
-    delay(50);                            //Delay for 0.05 seconds to 50 ms
-    digitalWrite(trigBack, LOW);          //Turn off send trigger signal to back ultrasonic sensor
-    duration2 = pulseIn(echoBack, HIGH);  //Set duration equal to the pulse reading from the echo of the back ultrasonic sensor
-    distance2 = (duration2/2)/29.1;         //Calculate the distance into cm
-    if(distance2 <= 10)                    //Check if the distance is less than or equal to 10 cm from the back
-    {
-      direction = 0;                      //Set direction to none to trigger a stop
-    }
+    // digitalWrite (trigBack, HIGH);        //Turn on send trigger signal to back ultrasonic sensor
+    // delay(50);                            //Delay for 0.05 seconds to 50 ms
+    // digitalWrite(trigBack, LOW);          //Turn off send trigger signal to back ultrasonic sensor
+    // duration = pulseIn(echoFront, HIGH);  //Set duration equal to the pulse reading from the echo of the back ultrasonic sensor
+    // distance = (duration/2)/29.1;         //Calculate the distance into cm
+    delay(7000);                          //Wait 3 seconds
+    direction = 0;                        //Set direction to none to trigger a stop
+    // if(distance <= 10)                    //Check if the distance is less than or equal to 10 cm from the back
+    // {
+    //   direction = 0;                      //Set direction to none to trigger a stop
+    // }
   }
   else{                                   //Check if direction is set as forward
-  //                                      //Wait for some verifcation from the raspberry pi
-    delay(5000);
-    direction = -1;
+    delay(10000);                         //Simulate 20 seconds of arm pulling out battery
+    direction = -1;                       //Set pod to move backwards
   }
 }
