47797c077c20311adfd6e5798a5e7d71c8b3781e
mi3night <mi3night2004@gmail.com>
2023-10-02 19:23:18 -0700

    serial look up

---
+++ Arm-Code/serialLookup.py
@@ -0,0 +1,26 @@
+import serial.tools.list_ports
+
+ports = serial.tools.list_ports.comports()
+serialInst = serial.Serial()
+
+portsList = []
+
+for onePort in ports:
+    portsList.append(str(onePort))
+    print(str(onePort))
+
+val = input("Select Port: COM")
+
+for x in range(0,len(portsList)):
+    if portsList[x].startswith("COM" + str(val)):
+        portVar = "COM" + str(val)
+        print(portVar)
+
+serialInst.baudrate = 9600
+serialInst.port = portVar
+serialInst.open()
+
+while True:
+	if serialInst.in_waiting:
+		packet = serialInst.readline()
+		print(packet.decode('utf').rstrip('\n'))
