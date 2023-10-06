f2dc57dec5ecf765c8e5da2bb08323ee9ed59f70
mi3night <mi3night2004@gmail.com>
2023-10-05 23:30:14 -0700

    2 way communication

--- P2P-TCPIP/samplehost.py
+++ P2P-TCPIP/samplehost.py
@@ -1,32 +1,25 @@
 import socket
 
-# Create a socket object
+HOST = '0.0.0.0'  # Listen on all available network interfaces
+PORT = 12345
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen()
 
-# Define the server's IP address and port
-server_ip = '0.0.0.0'  # Listen on all available interfaces
-server_port = 12345
+print(f"Server (Windows) is listening on {HOST}:{PORT}")
 
-# Bind the socket to the IP address and port
-server_socket.bind((server_ip, server_port))
-
-# Listen for incoming connections
-server_socket.listen(5)
-
-print(f"Server listening on {server_ip}:{server_port}")
-
-# Accept incoming connections
 client_socket, client_address = server_socket.accept()
-print(f"Connection from {client_address}")
+print(f"Connected to {client_address}")
 
-# Receive data from the client
-data = client_socket.recv(1024)
-print(f"Received: {data.decode('utf-8')}")
+while True:
+    data = client_socket.recv(1024)
+    if not data:
+        break
+    print(f"Received from Windows: {data.decode()}")
 
-# Send a response back to the client
-response = "Hello from Windows!"
-client_socket.send(response.encode('utf-8'))
+    response = input("Enter a response: ")
+    client_socket.send(response.encode())
 
-# Close the connection
 client_socket.close()
 server_socket.close()
