7e8eee49ca1b22b25bccfbba33524e822b5209f4
mi3night <mi3night2004@gmail.com>
2023-10-07 02:55:44 -0700

    upload summer code

--- Arm-Code/armarkertest.py
+++ Arm-Code/armarkertest.py
@@ -1,7 +1,6 @@
-
-
-
-
+import numpy as np
+import time
+import cv2
 import math
 import motorctrl_v1 as motor
 import Movement_Calc_v2 as calculation
@@ -9,6 +8,41 @@ import numpy as np
 import time
 import time
 import cv2
+
+ARUCO_DICT = {
+    "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+    "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+    "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+    "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+    "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+    "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+    "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+    "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+    "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+    "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+    "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+    "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+    "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+    "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+    "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+    "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+    "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+    "DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+    "DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+    "DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+    "DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+}
+
+
+aruco_type = "DICT_5X5_100"
+arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[aruco_type])
+arucoParams = cv2.aruco.DetectorParameters_create()
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+
 
 BASE_ID = 1
 BICEP_ID = 2
@@ -51,3 +85,63 @@ time.sleep(3)
 motor.simMotorRun([30, 227, 270, 47, 272], [0,1,2,3,4]) 
 time.sleep(7)
 motor.simMotorRun([30, 227, 301, 49, 143], [0,1,2,3,4]) 
+
+
+
+
+def aruco_display(corners, ids, rejected, image):
+    if len(corners) > 0:
+        ids = ids.flatten()
+        
+        for (markerCorner, markerID) in zip(corners, ids):
+            corners = markerCorner.reshape((4, 2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corners
+            
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+
+            cv2.line(image, topLeft, topRight, (255, 0, 255), 2)
+            cv2.line(image, topRight, bottomRight, (255, 0, 255), 2)
+            cv2.line(image, bottomRight, bottomLeft, (255, 0, 255), 2)
+            cv2.line(image, bottomLeft, topLeft, (255, 0, 255), 2)
+
+            #Object's center pixel coordinates    
+            h,w,_ = img.shape
+            global objX
+            global frameX
+            global objY
+            global frameY
+            fX=int(w/2)
+            frameX = fX
+            fY=int(h/2)
+            frameY = fY
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            objX = cX
+            cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+            objY = cY
+            cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+            objectCord = "(" + str(cX) + ", " + str(cY) + ")" 
+            cv2.putText(image, objectCord, (cX,cY), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,0,255), 2)
+            print("At pixel coordinates ({}, {})".format(cX,cY))
+
+            # Calculate distance
+            
+            marker_size = np.linalg.norm(np.array(topRight) - np.array(topLeft))
+            distance_feet, distance_per_pixel = calculate_distance(marker_size)
+            distance_feet_rounded = round(distance_feet, 2)
+            distance_per_pixel_rounded = round(distance_per_pixel, 6)
+            arX = distance_per_pixel * (cX - fX) / 3.6
+            arY = distance_feet_rounded
+            arX = round(arX, 4)
+            arY = round(arY, 4)
+            print("[Inference] ArUco marker ID: {}, Distance: {} feet, Distance per pixel: {} feet/pixel\n, X coord: {}, Y coord: {}\n".format(markerID, distance_feet_rounded, distance_per_pixel_rounded, arX, arY))
+            outlineText = "ID: " + str(markerID) + " at " +  str(distance_feet_rounded) + " feet, " +  str(distance_per_pixel_rounded) + " ft/pixel" 
+            outlineText2 = "X Axis: " + str(arX) + " Y Axis: " + str(arY)   
+            
+
+            cv2.putText(image, outlineText,(topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                0.6, (255, 0, 255), 2)
+            cv2.putText(image, outlineText2, (topLeft[0], topLeft[1] + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
+    return image
