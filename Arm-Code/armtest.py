1b2afd9eb97e70b92b5f9870bb22feaf4ff1ad23
mi3night <mi3night2004@gmail.com>
2023-10-07 00:32:47 -0700

    renamed files

--- Arm-Code/armtest.py
+++ Arm-Code/armtest.py
@@ -16,7 +16,7 @@ FOREARM_ID = 3
 WRIST_ID = 4
 CLAW_ID = 0
 
-PORT_NUM = '/dev/tty/USB0'
+PORT_NUM = '/dev/cu.usbserial-FT5NY9DI'
 
 BAUDRATE = 1000000
 
@@ -27,8 +27,17 @@ MOVE_IDs = [BASE_ID, BICEP_ID, FOREARM_ID, WRIST_ID, CLAW_ID]
 
 motor.portInitialization(PORT_NUM, ALL_IDs)
 motor.dxlSetVelo([20, 20, 20, 20, 20],[0, 1, 2, 3, 4]) #ALWAYS SET SPEED BEFORE ANYTHING
-motor.simMotorRun([25, 227, 270, 47, 272], [0,1,2,3,4]) #resting (not working) please test
-time.sleep(5)
+motor.simMotorRun([90, 223, 90, 222, 185], [0, 1, 2, 3, 4]) #set chamber
+time.sleep(4)
+motor.simMotorRun([31, 223, 90, 222, 190], [0, 1, 2, 3, 4]) #grab battery
+time.sleep(2)
+motor.dxlSetVelo([20, 20, 20, 40, 26],[0, 1, 2, 3, 4]) 
+motor.simMotorRun([129, 104, 285], [2, 3, 4]) #pull out
+motor.dxlSetVelo([20, 20, 20, 20, 20],[0, 1, 2, 3, 4])
+motor.simMotorRun([137, 62, 285], [2, 3, 4]) #pull out more
+time.sleep(3)
+motor.simMotorRun([30, 227, 270, 47, 272], [0,1,2,3,4]) #resting (not working) please test
+time.sleep(7)
 motor.simMotorRun([187], [2])  # back to pull down more
 time.sleep(3)
 motor.simMotorRun([150, 80], [2,3])
@@ -36,3 +45,9 @@ time.sleep(3)
 motor.dxlSetVelo([20, 20, 20, 40, 26],[0, 1, 2, 3, 4])
 motor.simMotorRun([25, 227, 130, 130, 250], [0, 1, 2, 3, 4])  #push in
 motor.simMotorRun([25, 227, 90, 222, 194], [0, 1, 2, 3, 4])
+time.sleep(3)
+motor.simMotorRun([90, 227, 90, 222, 194], [0, 1, 2, 3, 4])
+time.sleep(3)
+motor.simMotorRun([30, 227, 270, 47, 272], [0,1,2,3,4]) 
+time.sleep(7)
+motor.simMotorRun([30, 227, 301, 49, 143], [0,1,2,3,4]) 
