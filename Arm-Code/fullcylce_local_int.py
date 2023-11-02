f31bee8a6ee35dbbc352596544b246eae5e93bc5
OvAlexander <alexanderleeov@gmail.com>
2023-11-01 19:09:23 -0700

    added new arm code

--- Arm-Code/fullcylce_local_int.py
+++ Arm-Code/fullcylce_local_int.py
@@ -35,37 +35,59 @@ def pullout():
     print("pull out start")
     motor.dxlSetVelo([20, 20, 20, 20, 20], [0, 1, 2, 3, 4]
                      )  # ALWAYS SET SPEED BEFORE ANYTHING
-    motor.simMotorRun([90, 223, 90, 222, 185], [0, 1, 2, 3, 4])  # set chamber
-    time.sleep(4)
+    
+    motor.simMotorRun([110, 223, 270, 47, 272], [0, 1, 2, 3, 4])  # resting
+    
+    
+    checkMovement(MOVE_IDs)
+    motor.simMotorRun([187], [2])  # back to pull down more
+    checkMovement(MOVE_IDs)
+    motor.simMotorRun([150, 80], [2, 3])
+    checkMovement(MOVE_IDs)
+    motor.dxlSetVelo([20, 20, 20, 40, 26], [0, 1, 2, 3, 4])
+    motor.simMotorRun([110, 223, 130, 130, 250], [0, 1, 2, 3, 4])  # push in
+    motor.simMotorRun([110, 223, 90, 222, 194], [0, 1, 2, 3, 4])
+    checkMovement(MOVE_IDs)
+    motor.simMotorRun([110, 223, 90, 222, 194], [0, 1, 2, 3, 4])
+    checkMovement(MOVE_IDs)
+    
+ 
     motor.simMotorRun([31, 223, 90, 222, 190], [0, 1, 2, 3, 4])  # grab battery
-    time.sleep(2)
+    checkMovement(MOVE_IDs)
     motor.dxlSetVelo([20, 20, 20, 40, 26], [0, 1, 2, 3, 4])
     motor.simMotorRun([129, 104, 285], [2, 3, 4])  # pull out
     motor.dxlSetVelo([20, 20, 20, 20, 20], [0, 1, 2, 3, 4])
-    motor.simMotorRun([137, 62, 285], [2, 3, 4])  # pull out more
-    time.sleep(3)
-    motor.simMotorRun([30, 227, 270, 47, 272], [0, 1, 2, 3, 4])  # resting
-    time.sleep(7)
+    motor.simMotorRun([150, 62, 285], [2, 3, 4])  # pull out more
+    checkMovement(MOVE_IDs)
+    motor.simMotorRun([30, 223, 270, 47, 272], [0, 1, 2, 3, 4])  # resting
+    checkMovement(MOVE_IDs)
     print("pull out end")
 
 
 def pushin():
     print("push in start")
-    time.sleep(7)
-    motor.simMotorRun([187], [2])  # back to pull down more
-    time.sleep(3)
-    motor.simMotorRun([150, 80], [2, 3])
-    time.sleep(3)
-    motor.dxlSetVelo([20, 20, 20, 40, 26], [0, 1, 2, 3, 4])
-    motor.simMotorRun([25, 227, 130, 130, 250], [0, 1, 2, 3, 4])  # push in
-    motor.simMotorRun([25, 227, 90, 222, 194], [0, 1, 2, 3, 4])
-    time.sleep(3)
-    motor.simMotorRun([90, 227, 90, 222, 194], [0, 1, 2, 3, 4])
-    time.sleep(3)
-    motor.simMotorRun([30, 227, 270, 47, 272], [0, 1, 2, 3, 4])
-    time.sleep(7)
-    motor.simMotorRun([30, 227, 301, 49, 143], [0, 1, 2, 3, 4])
-    time.sleep(7)
+    checkMovement(MOVE_IDs)
+    motor.simMotorRun([180], [2])  # back to pull down more
+    checkMovement(MOVE_IDs)
+    motor.simMotorRun([178, 60], [2, 3])
+    checkMovement(MOVE_IDs)
+    motor.simMotorRun([30, 223, 130, 130, 250], [0, 1, 2, 3, 4])  # push in
+    motor.simMotorRun([30, 223, 90, 222, 210], [0, 1, 2, 3, 4])
+    checkMovement(MOVE_IDs)
+    motor.simMotorRun([110, 223, 90, 222, 194], [0, 1, 2, 3, 4])
+    checkMovement(MOVE_IDs)
+    
+ 
+    motor.simMotorRun([110, 223, 90, 222, 190], [0, 1, 2, 3, 4])  # grab battery
+    checkMovement(MOVE_IDs)
+    motor.dxlSetVelo([110, 20, 20, 40, 26], [0, 1, 2, 3, 4])
+    motor.simMotorRun([129, 104, 285], [2, 3, 4])  # pull out
+    motor.dxlSetVelo([20, 20, 20, 20, 20], [0, 1, 2, 3, 4])
+    motor.simMotorRun([150, 62, 285], [2, 3, 4])  # pull out more
+    checkMovement(MOVE_IDs)
+    motor.simMotorRun([110, 223, 270, 47, 272], [0, 1, 2, 3, 4])  # resting
+    checkMovement(MOVE_IDs)
+
     print("push in end")
 
 
