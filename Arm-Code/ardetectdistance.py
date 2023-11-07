79b5c910e4b5ec2bae05a40aa73639980f5f7075
mi3night <mi3night2004@gmail.com>
2023-11-07 01:33:56 -0800

    aaa

---
+++ Arm-Code/ardetectdistance.py
@@ -0,0 +1,42 @@
+import cv2
+import pyrealsense2
+from realsense_depth import *
+
+# Initialize Camera Intel Realsense
+dc = DepthCamera()
+
+# Create an AR marker detector (you may need to install the cv2_aruco package)
+aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_100)
+parameters = cv2.aruco.DetectorParameters_create()
+
+# Initialize the point of interest
+point = (400, 300)
+
+while True:
+    ret, depth_frame, color_frame = dc.get_frame()
+
+    # Detect AR markers in the color frame
+    corners, ids, _ = cv2.aruco.detectMarkers(color_frame, aruco_dict, parameters=parameters)
+
+    if ids is not None and len(ids) > 0:
+        # Get the center of the first detected AR marker
+        marker_center = corners[0][0].mean(axis=0).astype(int)
+
+        # Update the point based on the AR marker's center
+        point = tuple(marker_center)
+
+    # Show distance for the point of interest
+    cv2.circle(color_frame, point, 4, (0, 0, 255))
+    distance = depth_frame[point[1], point[0]]
+
+    cv2.putText(color_frame, "{}mm".format(distance), (point[0], point[1] - 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+
+    normalized_depth = cv2.normalize(depth_frame, None, 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+    colored_depth = cv2.applyColorMap(normalized_depth, cv2.COLORMAP_JET)
+    cv2.imshow("Depth Overlaid on Color", colored_depth)
+    cv2.imshow("depth frame", depth_frame)
+    cv2.imshow("Color frame", color_frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
--- Arm-Code/realsense_depth.py
+++ Arm-Code/realsense_depth.py
@@ -1,6 +1,8 @@
+import cv2
 import pyrealsense2 as rs
 import numpy as np
 
+# Add spatial and temporal filters to the pipeline configuration within the DepthCamera class
 class DepthCamera:
     def __init__(self):
         # Configure depth and color streams
@@ -16,21 +18,67 @@ class DepthCamera:
         config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
         config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-
+        # Create an align object for aligning depth and color frames
+        self.align = rs.align(rs.stream.color)
 
         # Start streaming
         self.pipeline.start(config)
 
+        # Add the spatial and temporal filters to the pipeline configuration
+        self.threshold_filter = rs.threshold_filter(0.3, 0.7)
+        self.temp_filter = rs.temporal_filter(0.1, 80.0, 6)
+        self.spat_filter = rs.spatial_filter(0.40, 40.0, 4.0, 5.0)  # test settings - 1st = aplha, 2nd = delta, 3rd = magniture, 4th 
+        # = holefilling(0 = none, 1 = 2px, 2 = 4px, 3 = 8px, 4 = 16px, 5 = unlimited)
+
+
+        
     def get_frame(self):
         frames = self.pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
 
+        # Align the frames to get the depth and color frames in sync
+        aligned_frames = self.align.process(frames)
+        aligned_depth_frame = aligned_frames.get_depth_frame()
+        color_frame = aligned_frames.get_color_frame()
+        depth_frame = frames.get_depth_frame()
+
+
+        if not aligned_depth_frame or not color_frame:
+            return False, None, None
+
+        # Apply hole-filling filter
+
+
+        # Convert frames to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
-        if not depth_frame or not color_frame:
-            return False, None, None
+
         return True, depth_image, color_image
 
     def release(self):
         self.pipeline.stop()
+
+    def hole_filling(self, depth_frame):
+        hole_filling = rs.hole_filling_filter()
+        return hole_filling.process(depth_frame)
+
+# Example usage
+if __name__ == "__main__":
+    dc = DepthCamera()
+    while True:
+        ret, depth_frame, color_frame = dc.get_frame()
+        if not ret:
+            break
+
+        # Normalize and apply a colormap to the depth frame
+        normalized_depth = cv2.normalize(depth_frame, None, 0, 255, cv2.NORM_MINMAX)
+        colored_depth = cv2.applyColorMap(normalized_depth, cv2.COLORMAP_JET)
+
+        cv2.imshow("Colored Depth frame", colored_depth)
+        cv2.imshow("Color frame", color_frame)
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
+    dc.release()
+    cv2.destroyAllWindows()
